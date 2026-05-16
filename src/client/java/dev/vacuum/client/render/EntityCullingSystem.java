52fae28d9dbab83bb59052daa75c1ad13ff22b43
bedspvp-glitch <bedspvp@gmail.com>
2026-05-16 12:11:04 +0200

    fix: complete truncated EntityCullingSystem.java

--- src/client/java/dev/vacuum/client/render/EntityCullingSystem.java
+++ src/client/java/dev/vacuum/client/render/EntityCullingSystem.java
@@ -18,4 +18,69 @@ public class EntityCullingSystem {
     private VacuumFrustum currentFrustum;
     private volatile int entityCount = 0;
 
-    publi
+    public void setFrustum(VacuumFrustum frustum) {
+        this.currentFrustum = frustum;
+    }
+
+    public void beginFrame() {
+        culledThisFrame.set(0);
+        renderedThisFrame.set(0);
+    }
+
+    /**
+     * Returns true if the entity should be rendered this frame.
+     */
+    public boolean shouldRender(Entity entity) {
+        VacuumConfig config = VacuumMod.getConfig();
+        if (!config.enableEntityCulling) {
+            return true;
+        }
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        if (client.player == null) {
+            return true;
+        }
+
+        // Always render the local player
+        if (entity == client.player) {
+            return true;
+        }
+
+        // Distance culling
+        Vec3d cameraPos = client.gameRenderer.getCamera().getPos();
+        double distSq = entity.squaredDistanceTo(cameraPos.x, cameraPos.y, cameraPos.z);
+        double maxDist = config.entityRenderDistanceMultiplier * 64.0;
+        if (distSq > maxDist * maxDist) {
+            culledThisFrame.incrementAndGet();
+            return false;
+        }
+
+        // Frustum culling
+        if (currentFrustum != null) {
+            Box box = entity.getBoundingBox().expand(0.5);
+            if (!currentFrustum.isVisible(box)) {
+                culledThisFrame.incrementAndGet();
+                return false;
+            }
+        }
+
+        renderedThisFrame.incrementAndGet();
+        return true;
+    }
+
+    public void endFrame() {
+        entityCount = renderedThisFrame.get() + culledThisFrame.get();
+    }
+
+    public int getCulledCount() {
+        return culledThisFrame.get();
+    }
+
+    public int getRenderedCount() {
+        return renderedThisFrame.get();
+    }
+
+    public int getTotalEntityCount() {
+        return entityCount;
+    }
+}
